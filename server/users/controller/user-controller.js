8212a1fba0629d73ee99dd7a71914566306b7d63
Daniele Cassia <danielecassia9@gmail.com>
2021-07-08 13:56:27 -0300

    Criamos rotas post e get

--- server/users/controller/user-controller.js
+++ server/users/controller/user-controller.js
@@ -2,6 +2,45 @@ const router = require('express').Router();
 
 const UserService = require('../service/UserService');
 
+router.post('/', async (req, res) => {
+  try {
+    const user = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      image: req.body.image,
+      role: 'user',
+    };
+
+    await UserService.createUser(user);
+
+    res.status(201).end();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await UserService.getAllUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+router.get('/user/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await UserService.getUserById(userId);
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.put('/user/:id', async (req, res) => {
   try {
     await UserService.updateUser(req.params.id, req.body);
--- server/users/service/UserService.js
+++ server/users/service/UserService.js
@@ -1,12 +1,25 @@
 const User = require('../model/User');
 
 class UserService {
-  async updateUser(id, body){
-    await User.update (body, {where: {id: id}});
+
+  async createUser(user) {
+    await User.create(user);
   }
 
-  async deleteUser(id){
-    await User.destroy({where: {id:id}});
+  async getAllUsers() {
+    return await User.findAll({ raw: true });
+  }
+
+  async getUserById(id) {
+    return await User.findByPk(id, { raw: true });
+  }
+
+  async updateUser(id, body) {
+    await User.update(body, { where: { id: id } });
+  }
+
+  async deleteUser(id) {
+    await User.destroy({ where: { id: id } });
   }
 }
 
