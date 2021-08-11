cfcfe7e456076cf9a3ccaed7650a58a9640a1058
Henrique César <rickcesaralves07@gmail.com>
2021-08-11 12:34:33 -0300

    Funcionalidade e visual perfil

--- server/react-app/src/components/Home/PerfilAluno/PerfilAluno.jsx
+++ server/react-app/src/components/Home/PerfilAluno/PerfilAluno.jsx
@@ -16,17 +16,14 @@ export default function PerfilAluno() {
     axios.get(`/users/user/${id}`)
       .then( (res) => setSelectedUser(res.data) )
       .catch( (err) => console.log(err.response) ),
-  [id]);
-
-  var data_nascFormatada = new Date();
-
+  [id])
+  
   return(
     <div className="PerfilAluno">
       <Container fixed className ="containerPerfil">
         <Row className = "rowDados">
 
           <Col xs lg="2" className = "ColunaImg">
-            <Card className="cardPerfil">
               <Card.Title className="FotoPerfilAluno" >
                 <img src={Logo} alt="Foto de Perfil" width="100%"/>
                 <br />
@@ -35,39 +32,30 @@ export default function PerfilAluno() {
                   <Button variant="warning">Adicionar Professor</Button>    
                 </Card.Text>
               </Card.Title>
-            </Card>
           </Col>
 
           <Col xs={1}></Col>
           
-          <Col xs lg="4"  className = "ColunaDados">
-            <Card className="cardPerfil">
+          <Col xs lg="4" className = "ColunaDados">
                 <Card.Text className="cardPerfil">
                   <p> Nome: {selectedUser ? selectedUser.name : ''} </p> 
                   <p> Email: {selectedUser ? selectedUser.email : ''} </p> 
                   <p> Data de nascimento: {selectedUser ? selectedUser.data_nasc : ''} </p> 
                   <p> Instrumento: Violão </p> 
                   <div className="botoesFormPerfil">
-                    <Button variant="info">Editar</Button>   
-                    <Button variant="success">Salvar</Button>    
-                    <Button variant="danger">Cancelar</Button>    
+                    <Button variant="primary">Editar</Button>   
+                    {/* <Button variant="danger">Cancelar</Button>    
+                    <Button variant="success">Salvar</Button>     */}
                   </div>
                 </Card.Text>
-            </Card>
 
           </Col>
         </Row>
 
-
-        <Row className="tabelaPerfil">
-          <Col sm></Col>
-          <Col sm>
-            <h2> Professores </h2>
-            <ListaProfessores/>
-          </Col>
-          <Col sm></Col>
+        <Row className="rowPerfil">
+          <h2> Professores </h2>
+          <ListaProfessores/>
         </Row>
-
       </Container>
     </div>    
   )
